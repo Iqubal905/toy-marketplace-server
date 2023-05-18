753608faf8cd4d123650b5747fa1b3a613b32143
Iqubal905 <iqubal905@gmail.com>
2023-05-19 02:01:33 +0600

    postApiAdd

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 
 
 
-console.log(process.env.DB_PASSWORD);
+
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.f4mhroj.mongodb.net/?retryWrites=true&w=majority`;
@@ -31,12 +31,20 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const toyCollection = client.db('toySeller').collection('toys')
+    app.post('/addtoys', async (req, res) =>{
+        const toys = req.body
+       console.log(toys);
+       const result = await toyCollection.insertOne(toys);
+       res.send(result)
+    })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    
   }
 }
 run().catch(console.dir);
